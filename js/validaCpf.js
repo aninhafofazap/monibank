b38ec5442ed8e234928f7f3d660aae9fd107faff
Ana Victoria <anavictoriagomes1810@gmail.com>
2023-01-08 22:26:07 -0300

    Imprimindo as mensagens de erro dos campos

--- js/validaCpf.js
+++ js/validaCpf.js
@@ -1,66 +1,66 @@
-
 // Função para verificar se o cpf existe ou não
 export default function umCpf(campo) {
-    const cpf = campo.value.replace(/\.|-/g, "");
-    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse CPF não existe!")
-    } else {
-        console.log("Esse CPF existe")
-    }
+  const cpf = campo.value.replace(/\.|-/g, "");
+  if (
+    validaNumerosRepetidos(cpf) ||
+    validaPrimeiroDigito(cpf) ||
+    validaSegundoDigito(cpf)
+  ) {
+    campo.setCustomValidity("Esse CPF não é válido");
+  }
 }
 
 function validaNumerosRepetidos(cpf) {
-    const numerosRepetidos = [
-        '00000000000',
-        '11111111111',
-        '22222222222',
-        '33333333333',
-        '44444444444',
-        '55555555555',
-        '66666666666',
-        '77777777777',
-        '88888888888',
-        '99999999999'
-    ]
+  const numerosRepetidos = [
+    "00000000000",
+    "11111111111",
+    "22222222222",
+    "33333333333",
+    "44444444444",
+    "55555555555",
+    "66666666666",
+    "77777777777",
+    "88888888888",
+    "99999999999",
+  ];
 
-    return numerosRepetidos.includes(cpf);
+  return numerosRepetidos.includes(cpf);
 }
 
 // Fazendo um laço de repetição para pegar todos os digitos, fazendo com que fiquem looping juntos.
 
 function validaPrimeiroDigito(cpf) {
-    let soma = 0;
-    let multiplicador = 10;
+  let soma = 0;
+  let multiplicador = 10;
 
-    for (let tamanho = 0; tamanho < 9; tamanho++) {
-        soma += cpf[tamanho] * multiplicador;
-        multiplicador--
-    }
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
 
-    soma = (soma * 10) % 11;
+  soma = (soma * 10) % 11;
 
-    if (soma == 10 || soma == 11) {
-        soma = 0;
-    }
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
 
-    return soma != cpf[9]
+  return soma != cpf[9];
 }
 
-
 function validaSegundoDigito(cpf) {
-    let soma = 0;
-    let multiplicador = 11;
+  let soma = 0;
+  let multiplicador = 11;
 
-    for (let tamanho = 0; tamanho < 10; tamanho++) {
-        soma += cpf[tamanho] * multiplicador;
-        multiplicador--;
-    }
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
 
-    soma = (soma * 10) % 11;
+  soma = (soma * 10) % 11;
 
-    if (soma == 10 || soma == 11) {
-        soma = 0;
-    }
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
 
-    return soma != cpf[10];
+  return soma != cpf[10];
 }
--- js/validaIdade.js
+++ js/validaIdade.js
@@ -1,14 +1,18 @@
 // Criando verificação pra idade.
 export default function maiorDeIdade(campo) {
-    const dataNascimento = new Date(campo.value);
-    validaIdade(dataNascimento);
-
-    console.log(validaIdade(dataNascimento));
+  const dataNascimento = new Date(campo.value);
+  if (!validaIdade(dataNascimento)) {
+    campo.setCustomValidity("O usuário não é maior de idade");
+  }
 }
 
 function validaIdade(data) {
-    const dataAtual = new Date();
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+  const dataAtual = new Date();
+  const dataMais18 = new Date(
+    data.getUTCFullYear() + 18,
+    data.getUTCMonth(),
+    data.getUTCDate()
+  );
 
-    return dataAtual >= dataMais18;
+  return dataAtual >= dataMais18;
 }
